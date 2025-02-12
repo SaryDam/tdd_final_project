2f2349fef338c73197080d3e11eab61de08b4b81
dcreux <d.creux@groupe-burus.tech>
2025-02-12 15:30:10 +0100

    test d'initialisation du tableau

--- src/dames.ts
+++ src/dames.ts
@@ -1,3 +1,11 @@
 export function nQueens(n: number): string[][] {
     return [];
 }
+
+export function initializeBoard(n: number): string[][] {
+    const board: string[][] = [];
+    for (let i = 0; i < n; i++) {
+        board.push(Array(n).fill('.'));
+    }
+    return board;
+}
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,20 @@
-import {nQueens} from "./dames";
+import {initializeBoard, nQueens} from "./dames";
 
 describe('nQueens', () => {
     it('retourne un tableau vide, si n = 0', () => {
         expect(nQueens(0)).toEqual([]);
     });
+    it.each([
+        [1, [['.']]],
+        [2, [['.', '.'], ['.', '.']]],
+        [3, [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]],
+        [4, [['.', '.', '.', '.'], ['.', '.', '.', '.'], ['.', '.', '.', '.'], ['.', '.', '.', '.']]],
+    ])(
+        'initialisation du tableau pour n = %i',
+        (n, expectedBoard) => {
+            const board = initializeBoard(n);
+            expect(board).toEqual(expectedBoard);
+        }
+    );
+
 })
